ba7e536ca208eded21d757302e08719d320c9fe7
brianchanoocl <Delubdi118>
2021-12-16 19:29:28 +0800

    [todo_list_backend] [Brian] -feat: create and implement controller test case should_remove_todo_item_when_perform_delete_given_todo_items_and_id()

--- src/test/java/com/example/todoList/controller/TodoControllerTest.java
+++ src/test/java/com/example/todoList/controller/TodoControllerTest.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,6 +47,19 @@ public class TodoControllerTest {
                 .andExpect(jsonPath("$[0].id").isString())
                 .andExpect(jsonPath("$[0].text").value("task 1"))
                 .andExpect(jsonPath("$[0].done").value(false));
+    }
 
+    @Test
+    void should_remove_todo_item_when_perform_delete_given_todo_items_and_id() throws Exception {
+        //given
+        TodoItem todoItem = new TodoItem("1","task 1",false);
+        todoRepositoryNew.insert(todoItem);
+
+        //When
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/todos/"+todoItem.getId()))
+                .andExpect(status().isNoContent());
+
+        assertEquals(0,todoRepositoryNew.findAll().size());
     }
 }
